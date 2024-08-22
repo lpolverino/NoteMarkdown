5885f5066702f9a46c777bab785b60122d312c29
lpolverino <lorenz.polverino@gmail.com>
2024-08-22 20:31:05 -0300

    Added gitignore & file uploading

--- api.go
+++ api.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 )
@@ -23,10 +27,38 @@ func (a *Api) run() {
 	e.POST("/note", func(c echo.Context) error {
 
 		contentType := c.Request().Header.Get("Content-Type")
+		fmt.Println(contentType)
 
-		if contentType != "multipart/form-data" {
+		if !strings.Contains(contentType, "multipart/form-data") {
+			fmt.Println("here")
 			return http.ErrNotMultipart
 		}
+
+		file, err := c.FormFile("file")
+		if err != nil {
+			fmt.Printf("cannot load file, %v\n", err)
+			return err
+		}
+
+		src, err := file.Open()
+		if err != nil {
+			fmt.Printf("cannot open file, %v\n", err)
+			return err
+		}
+		defer src.Close()
+
+		dst, err := os.Create(file.Filename)
+		if err != nil {
+			fmt.Printf("cannot create file, %v\n", err)
+			return err
+		}
+		defer dst.Close()
+
+		if _, err := io.Copy(dst, src); err != nil {
+			fmt.Printf("cannot copy the file, %v", err)
+			return err
+		}
+
 		return c.String(http.StatusOK, "Note Saves")
 	})
 
